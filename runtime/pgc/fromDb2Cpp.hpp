32687fd71b8ea4da00a1e68874ab67babf9e6544
vopl <devnull@localhost>
2011-05-18 00:00:00 +0400

    матрица преобразования типов в процессе

--- runtime/pgc/fromDb2Cpp.hpp
+++ runtime/pgc/fromDb2Cpp.hpp
@@ -2,18 +2,428 @@
 #define _PGC_FROMDB2CPP_HPP_
 
 #include "cppType.hpp"
+#include <pgtypes_numeric.h>
+#include <vector>
+#include <strstream>
 
 namespace pgc
 {
 	//////////////////////////////////////////////////////////////////////////
+	template <class T>
+	inline T &revert(T &v)
+	{
+		unsigned char *data = (unsigned char *)&v;
+		int len = sizeof(T);
+		size_t len_2 = len/2;
+		for(size_t i=0; i<len_2; i++)
+		{	
+			size_t i2 = len - i - 1;
+			unsigned char b = data[i];
+			data[i] = data[i2];
+			data[i2] = b;
+		}
+		return v;
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	namespace impl
+	{
+		template <int maxDecs, typename integral>
+		char *_ntoa(integral num, char *res)
+		{
+			if(num)
+			{
+				bool negative = num<0;
+				int pos=maxDecs;
+
+				res[pos] = 0;
+
+				if(negative)
+				{
+					while(num)
+					{
+						res[--pos] = (char)('0' - (num % 10));
+						num /= 10;
+					}
+					res[--pos] = '-';
+				}
+				else
+				{
+					while(num)
+					{
+						res[--pos] = (char)('0' + (num % 10));
+						num /= 10;
+					}
+				}
+				if(pos)
+				{
+					memmove(res, res+pos, maxDecs-pos+1);
+				}
+				return res;
+			}
+			res[0] = '0';
+			res[1] = 0;
+			return res;
+		}
+		template <int maxDecs, typename integral>
+		char *_untoa(integral num, char *res)
+		{
+			if(num)
+			{
+				int pos=maxDecs;
+				res[pos] = 0;
+
+				while(num)
+				{
+					res[--pos] = (char)('0' + (num % 10));
+					num /= 10;
+				}
+				if(pos)
+				{
+					memmove(res, res+pos, maxDecs-pos+1);
+				}
+				return res;
+			}
+			res[0] = '0';
+			res[1] = 0;
+			return res;
+		}
+
+		inline
+			char *_ntoa(long long num, char *res)
+		{
+			return impl::_ntoa<24>(num, res);
+		}
+		inline
+			char *_ntoa(unsigned long long num, char *res)
+		{
+			return impl::_untoa<25>(num, res);
+		}
+		inline
+			char *_ntoa(long num, char *res)
+		{
+			return impl::_ntoa<12>(num, res);
+		}
+		inline
+			char *_ntoa(unsigned long num, char *res)
+		{
+			return impl::_untoa<13>(num, res);
+		}
+		inline
+			char *_ntoa(int num, char *res)
+		{
+			return impl::_ntoa<12>(num, res);
+		}
+		inline
+			char *_ntoa(unsigned int num, char *res)
+		{
+			return impl::_untoa<13>(num, res);
+		}
+		inline
+			char *_ntoa(short num, char *res)
+		{
+			return impl::_ntoa<6>(num, res);
+		}
+		inline
+			char *_ntoa(unsigned short num, char *res)
+		{
+			return impl::_untoa<7>(num, res);
+		}
+		inline
+			char *_ntoa(char num, char *res)
+		{
+			return impl::_ntoa<3>(num, res);
+		}
+		inline
+			char *_ntoa(unsigned char num, char *res)
+		{
+			return impl::_untoa<4>(num, res);
+		}
+
+	}
+
+
+
+
+	//////////////////////////////////////////////////////////////////////////
+#define TMODULO(t,q,u) \
+	do { \
+	(q) = (((t) < 0) ? ceil((t) / (u)): floor((t) / (u))); \
+	if ((q) != 0) (t) -= int((q) * (u) + 0.5); \
+	} while(0)
+
+	typedef double TTimestamp;
+	enum
+	{
+		SECS_PER_YEAR	= 36525 * 864,	/* avoid floating-point computation */
+		SECS_PER_DAY	= 86400,
+		SECS_PER_HOUR	= 3600,
+		SECS_PER_MINUTE = 60,
+		MINS_PER_HOUR	= 60,
+	};
+
+	int date2j(int y, int m, int d);
+	void j2date(int jd, int *year, int *month, int *day);
+
+	double time2j(const int hour, const int min, const int sec, const int fsec);
+	void j2time(double t, int *hour, int *min, int *sec, int *fsec);
+
+
+
+	//////////////////////////////////////////////////////////////////////////
+	inline int date2j(int y, int m, int d)
+	{
+		int			julian;
+		int			century;
+
+		if (m > 2)
+		{
+			m += 1;
+			y += 4800;
+		}
+		else
+		{
+			m += 13;
+			y += 4799;
+		}
+
+		century = y / 100;
+		julian = y * 365 - 32167;
+		julian += y / 4 - century + century / 4;
+		julian += 7834 * m / 256 + d;
+
+		return julian;
+	}
+
+
+	inline void j2date(int jd, int *year, int *month, int *day)
+	{
+		unsigned int julian;
+		unsigned int quad;
+		unsigned int extra;
+		int			y;
+
+		julian = jd;
+		julian += 32044;
+		quad = julian / 146097;
+		extra = (julian - quad * 146097) * 4 + 3;
+		julian += 60 + quad * 3 + extra / 146097;
+		quad = julian / 1461;
+		julian -= quad * 1461;
+		y = julian * 4 / 1461;
+		julian = ((y != 0) ? (julian + 305) % 365 : (julian + 306) % 366) + 123;
+		y += quad * 4;
+		*year = y - 4800;
+		quad = julian * 2141 / 65536;
+		*day = julian - 7834 * quad / 256;
+		*month = (quad + 10) % 12 + 1;
+
+		return;
+	}
+
+
+	//////////////////////////////////////////////////////////////////////////
+	inline double time2j(const int hour, const int min, const int sec, const int fsec)
+	{
+		double d=0;
+		int i = fsec;
+		while(i)
+		{
+			d += i%10;
+			d /= 10;
+			i /= 10;
+		}
+		return (((((double)hour * MINS_PER_HOUR) + min) * SECS_PER_MINUTE) + sec) + d;
+	}
+
+	inline void j2time(double t, int *hour, int *min, int *sec, int *fsec)
+	{
+		*hour = (int)(t / SECS_PER_HOUR);
+		t -= (*hour) * SECS_PER_HOUR;
+		*min = (int)(t / SECS_PER_MINUTE);
+		t -= (*min) * SECS_PER_MINUTE;
+		*sec = (int)(t);
+		t -= *sec;
+		*fsec = 0;
+		while(t)
+		{
+			*fsec *= 10;
+			t *= 10;
+			int d = (int)t;
+			*fsec += d;
+			t -= d;
+		}
+
+	}
+
+
+
+	//////////////////////////////////////////////////////////////////////////
 	bool fromDb2Cpp(char *&valCpp, Oid typDb, int lenDb, const char *valDb)
 	{
-// 		switch(typDb)
-// 		{
-// 
-// 		}
-		assert(0);
-		return false;
+		switch(typDb)
+		{
+		case 21://int2
+			{
+				boost::int16_t v = *(boost::int16_t *)valDb;
+				impl::_ntoa(revert(v), valCpp);
+			}
+			break;
+		case 23://int4
+			{
+				boost::int32_t v = *(boost::int32_t *)valDb;
+				impl::_ntoa(revert(v), valCpp);
+			}
+			break;
+		case 20://int8
+			{
+				boost::int64_t v = *(boost::int64_t *)valDb;
+				impl::_ntoa(revert(v), valCpp);
+			}
+			break;
+		case 1700://numeric
+			{
+				typedef struct NumericData
+				{
+					short ndigits;
+					short weight;
+					short sign;
+					short dscale;
+					short NumericDigits[1];
+				} NumericData;
+
+				std::vector<unsigned char> buf(lenDb);
+				memcpy(&buf.front(), valDb, lenDb);
+				NumericData &v = *(NumericData *)&buf.front();
+
+				revert(v.ndigits);
+				revert(v.weight);
+				revert(v.sign);
+				revert(v.dscale);
+
+				size_t valCppPos = 0;
+				if(v.sign)
+				{
+					valCpp[valCppPos++] = '-';
+				}
+
+				for(int idx(0); idx<v.ndigits; idx++)
+				{
+					revert(v.NumericDigits[idx]);
+					char numStrPart[8];
+					size_t numStrPartPos=7;
+					numStrPart[numStrPartPos--] = 0;
+					while(v.NumericDigits[idx])
+					{
+						numStrPart[numStrPartPos--] = char('0'+v.NumericDigits[idx]%10);
+						v.NumericDigits[idx] /= 10;
+					}
+
+					memcpy(valCpp+valCppPos, numStrPart+numStrPartPos+1, 7-numStrPartPos-1);
+					valCppPos += 7-numStrPartPos-1;
+
+					if(v.weight>=0)
+					{
+						if(!v.weight)
+						{
+							valCpp[valCppPos++] = '.';
+						}
+						v.weight--;
+					}
+				}
+
+				while('0' == valCpp[--valCppPos]);
+				valCpp[++valCppPos] = 0;
+			}
+			break;
+
+		case 700://float4
+			{
+				float v = *(float *)valDb;
+				revert(v);
+				sprintf(valCpp,"%.7g",(double)v);
+			}
+			break;
+		case 701://float8
+			{
+				double v = *(double *)valDb;
+				revert(v);
+				sprintf(valCpp,"%.16g",v);
+			}
+			break;
+		case 790://money
+			{
+				boost::uint64_t v = *(boost::uint64_t *)valDb;
+				impl::_ntoa(revert(v), valCpp);
+			}
+			break;
+		case 1043://varchar
+			{
+				memcpy(valCpp, valDb, lenDb);
+				valCpp[lenDb] = 0;
+			}
+			break;
+		case 1042://bpchar
+			{
+				memcpy(valCpp, valDb, lenDb);
+				valCpp[lenDb] = 0;
+			}
+			break;
+		case 25://text
+			{
+				memcpy(valCpp, valDb, lenDb);
+				valCpp[lenDb] = 0;
+			}
+			break;
+		case 17://bytea
+			{
+				size_t lenCpp=0;
+				unsigned char *esc = PQescapeBytea((const unsigned char *)valDb, lenDb, &lenCpp);
+				memcpy(valCpp, esc, lenCpp);
+				valCpp[lenCpp] = 0;
+			}
+			break;
+		case 1114://timestamp
+			{
+
+				double v = *(double *)valDb;
+				revert(v);
+
+				double		date;
+				double		time;
+
+				time = v;
+				TMODULO(time, date, (double) SECS_PER_DAY);
+
+				if (time < 0)
+				{
+					time += SECS_PER_DAY;
+					date -= 1;
+				}
+
+				/* add offset to go from J2000 back to standard Julian date */
+				date += 2451545;
+
+				/* Julian day routine does not work for negative Julian days */
+				int year, month, day, hour, min, sec, fsec;
+				j2date((int) date, &year, &month, &day);
+				j2time(time, &hour, &min, &sec, &fsec);
+
+				int k=220;
+
+
+			}
+			break;
+
+
+		default:
+			assert(!"unsupported db type");
+			valCpp[0] = 0;
+			return false;
+
+		}
+
+		return true;
 	}
 
 	//////////////////////////////////////////////////////////////////////////
--- runtime/pgc/pgc.cpp
+++ runtime/pgc/pgc.cpp
@@ -7,16 +7,156 @@
 
 #include "connection.hpp"
 #include "exception.hpp"
+#include <boost/cstdint.hpp>
+
+
+void createTestTable(pgc::Connection con)
+{
+	//////////////////////////////////////////////////////////////////////////
+	con.once().sql("DROP TYPE mood CASCADE").exec();
+	con.once().sql("DROP TABLE pgc_test CASCADE").exec();
+
+	con.once().sql("CREATE TYPE mood AS ENUM ('sad', 'ok', 'happy')").exec().throwIfError();
+
+	con.once().sql(
+		"CREATE TABLE pgc_test("
+		"	id SERIAL NOT NULL,"
+
+		// smallint 2 bytes small-range integer -32768 to +32767 
+		"	_smallint smallint,"
+		// integer 4 bytes usual choice for integer -2147483648 to +2147483647 
+		"	_integer integer,"
+		// bigint 8 bytes large-range integer -9223372036854775808 to 9223372036854775807 
+		"	_bigint bigint,"
+		// decimal variable user-specified precision, exact no limit 
+		"	_decimal decimal(11,6),"
+		// numeric variable user-specified precision, exact no limit 
+		"	_numeric numeric(10,7),"
+		// real 4 bytes variable-precision, inexact 6 decimal digits precision 
+		"	_real real,"
+		// double precision 8 bytes variable-precision, inexact 15 decimal digits precision 
+		"	_double double precision,"
+		// money 8 bytes currency amount -92233720368547758.08 to +92233720368547758.07 
+		"	_money money,"
+		// character varying(n), varchar(n)  variable-length with limit 
+		"	_varchar varchar,"
+		// character(n), char(n)  fixed-length, blank padded 
+		"	_char char(10),"
+		// text variable unlimited length 
+		"	_text text,"
+		// bytea 1 or 4 bytes plus the actual binary string variable-length binary string 
+		"	_bytea bytea,"
+		// timestamp [ (p) ] [ without time zone ] 8 bytes both date and time 4713 BC 5874897 AD 1 microsecond / 14 digits 
+		"	_timestamp timestamp,"
+		// timestamp [ (p) ] with time zone 8 bytes both date and time, with time zone 4713 BC 5874897 AD 1 microsecond / 14 digits 
+		"	_timestamptz timestamp with time zone,"
+		// interval [ (p) ] 12 bytes time intervals -178000000 years 178000000 years 1 microsecond / 14 digits 
+		"	_interval interval,"
+		// date 4 bytes dates only 4713 BC 5874897 AD 1 day 
+		"	_date date,"
+		// time [ (p) ] [ without time zone ] 8 bytes times of day only 00:00:00 24:00:00 1 microsecond / 14 digits 
+		"	_time time,"
+		// time [ (p) ] with time zone 12 bytes times of day only, with time zone 00:00:00+1459 24:00:00-1459 1 microsecond / 14 digits 
+		"	_timetz time with time zone,"
+		// boolean can have one of only two states: “true” or “false”. 
+		"	_boolean boolean,"
+		// enum mood
+		"	_mood mood,"
+		// There are two SQL bit types: bit(n) and 
+		"	_bit bit(10),"
+		// bit varying(n), where n is a positive integer
+		"	_varbit varbit,"
+		// oid
+		"	_oid oid,"
+
+
+		"	CONSTRAINT pgc_test_pkey PRIMARY KEY (id)"
+		");").exec().throwIfError();
+
+
+	con.once().sql(
+		"INSERT INTO pgc_test("
+		"_smallint,_integer,_bigint,_numeric, _decimal, _real, _double, _money, _varchar, _char, _text, _bytea, _timestamp, _timestamptz, _interval, _date, _time, _timetz, _boolean, _mood, _bit, _varbit, _oid"
+		")VALUES("
+		"1,2,3,12.23445,55654.69,3.123,4.123,'1000,32'::money,'varchar text','char txt','text text',E'\\17601234\\010','2010-04-08 01:02:03','2010-01-15 23:23:32','1 12:59:10','2010-01-15','23:23:32','23:23:32',true,'ok',B'1010101010',B'10101',220"
+		")").exec().throwIfError();
+}
+
+//////////////////////////////////////////////////////////////////////////
+void selectTestTable(pgc::Connection con)
+{
+	pgc::Result res = con.once().sql("SELECT "
+		"_smallint,_integer,_bigint,_numeric, _decimal, _real, _double, _money, _varchar, _char, _text, _bytea, _timestamp, _timestamptz, _interval, _date, _time, _timetz, _boolean, _mood, _bit, _varbit, _oid"
+		" FROM pgc_test").exec().throwIfError();
+
+	{
+		char buf[4096];
+		char *			v_pchar = buf;
+
+		res.fetch(0, "_smallint", v_pchar);
+		res.fetch(0, "_integer", v_pchar);
+		res.fetch(0, "_bigint", v_pchar);
+		res.fetch(0, "_numeric", v_pchar);
+		res.fetch(0, "_decimal", v_pchar);
+		res.fetch(0, "_real", v_pchar);
+		res.fetch(0, "_double", v_pchar);
+		res.fetch(0, "_money", v_pchar);
+		res.fetch(0, "_varchar", v_pchar);
+		res.fetch(0, "_char", v_pchar);
+		res.fetch(0, "_text", v_pchar);
+		res.fetch(0, "_bytea", v_pchar);
+		res.fetch(0, "_timestamp", v_pchar);
+		res.fetch(0, "_timestamptz", v_pchar);
+		res.fetch(0, "_interval", v_pchar);
+		res.fetch(0, "_date", v_pchar);
+		res.fetch(0, "_time", v_pchar);
+		res.fetch(0, "_timetz", v_pchar);
+		res.fetch(0, "_boolean", v_pchar);
+		res.fetch(0, "_mood", v_pchar);
+		res.fetch(0, "_bit", v_pchar);
+		res.fetch(0, "_varbit", v_pchar);
+		res.fetch(0, "_oid", v_pchar);
+	}
+
+
+	std::string		v_stdstring;
+	bool			v_bool;
+	boost::int8_t	v_int8;
+	boost::int16_t	v_int16;
+	boost::int32_t	v_int32;
+	boost::int64_t	v_int64;
+	boost::uint8_t	v_uint8;
+	boost::uint16_t	v_uint16;
+	boost::uint32_t	v_uint32;
+	boost::uint64_t	v_uint64;
+	std::tm			v_stdtm;
+
+// 	res.fetch(0, "_smallint", v_stdstring);
+// 	res.fetch(0, "_smallint", v_bool);
+// 	res.fetch(0, "_smallint", v_int8);
+// 	res.fetch(0, "_smallint", v_int16);
+// 	res.fetch(0, "_smallint", v_int32);
+// 	res.fetch(0, "_smallint", v_int64);
+// 	res.fetch(0, "_smallint", v_uint8);
+// 	res.fetch(0, "_smallint", v_uint16);
+// 	res.fetch(0, "_smallint", v_uint32);
+// 	res.fetch(0, "_smallint", v_uint64);
+// 	res.fetch(0, "_smallint", v_stdtm);
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	pgc::Connection con;
-	con.open("dbname=test user=postgres password=postgres");
+	con.open("dbname=test user=postgres password=postgres port=5433");
+
 
 	std::string str;
 
 	try
 	{
+		createTestTable(con);
+		selectTestTable(con);
+
 // 		int i;
 // 		con.once().sql("SELECT $1::varchar")
 // 			.bind(i)
