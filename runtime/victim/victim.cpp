15a85584d972c956127b1adbcf51b3012ce0516b
vopl <devnull@localhost>
2011-06-28 00:00:00 +0400

    .

--- runtime/victim/victim.cpp
+++ runtime/victim/victim.cpp
@@ -2,10 +2,17 @@
 //
 
 #include "stdafx.h"
+#include "litesql/expr.hpp"
+using namespace litesql;
+#include <iostream>
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	FieldType f1("f1", "typ1", "tbl1");
+	FieldType f2("f2", "typ2", "tbl2");
+
+	std::cout<<(f1 == 2).asString()<<std::endl;
 	return 0;
 }
 
