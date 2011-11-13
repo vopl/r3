db3e1527069b27fe8cd100ee7a2f8323c6cb048a
vopl <devnull@localhost>
2011-11-14 00:00:00 +0400

    .

--- runtime/include/utils/data.hpp
+++ runtime/include/utils/data.hpp
@@ -8,49 +8,161 @@
 
 #include <vector>
 #include <map>
-#include <boost/variant.hpp>
 
-namespace impl
+namespace utils
 {
+	enum EVariantType
+	{
+		evtNull,
+
+		// std::string
+		evtString,
+		// float
+		evtFloat,
+		// double
+		evtDouble,
+		// boost::int8_t
+		evtInt8,
+		// boost::int16_t
+		evtInt16,
+		// boost::int32_t
+		evtInt32,
+		// boost::int64_t
+		evtInt64,
+		// boost::uint8_t
+		evtUInt8,
+		// boost::uint16_t
+		evtUInt16,
+		// boost::uint32_t
+		evtUInt32,
+		// boost::uint64_t
+		evtUInt64,
+		// std::vector<char>
+		evtVectorChar,
+		// boost::gregorian::date
+		evtDate,
+		// boost::posix_time::ptime
+		evtTime,
+
+		// std::vector<Variant>
+		evtVectorVariant,
+
+		// std::map<std::string, Variant>
+		evtMapStringVariant,
+	};
+
+
 	class Variant
-		: public boost::make_recursive_variant <
-
-		std::string
-		, float
-		, double
-		, boost::int8_t
-		, boost::int16_t
-		, boost::int32_t
-		, boost::int64_t
-		, boost::uint8_t
-		, boost::uint16_t
-		, boost::uint32_t
-		, boost::uint64_t
-		, std::vector<char>
-		, boost::gregorian::date
-		, boost::posix_time::ptime
-		, std::vector<Variant>
-		, std::map<std::string, Variant>
-
-		> ::type
 	{
 	public:
+		typedef std::string 					String;
+		typedef float							Float;
+		typedef double							Double;
+		typedef boost::int8_t					Int8;
+		typedef boost::int16_t					Int16;
+		typedef boost::int32_t					Int32;
+		typedef boost::int64_t					Int64;
+		typedef boost::uint8_t					UInt8;
+		typedef boost::uint16_t					UInt16;
+		typedef boost::uint32_t					UInt32;
+		typedef boost::uint64_t					UInt64;
+		typedef std::vector<char>				VectorChar;
+		typedef boost::gregorian::date			Date;
+		typedef boost::posix_time::ptime		Time;
+		typedef std::vector<Variant>			VectorVariant;
+		typedef std::map<std::string, Variant>	MapStringVariant;
+
+
+	public:
+		~Variant();
+
 		Variant();
-		Variant(const Variant &);
-		template<typename T> Variant(const T &);
+		Variant(const Variant &v);
+		Variant(const String &v);
+		Variant(const Float &v);
+		Variant(const Double &v);
+		Variant(const Int8 &v);
+		Variant(const Int16 &v);
+		Variant(const Int32 &v);
+		Variant(const Int64 &v);
+		Variant(const UInt8 &v);
+		Variant(const UInt16 &v);
+		Variant(const UInt32 &v);
+		Variant(const UInt64 &v);
+		Variant(const VectorChar &v);
+		Variant(const Date &v);
+		Variant(const Time &v);
+		Variant(const VectorVariant &v);
+		Variant(const MapStringVariant &v);
+
+		Variant &operator=(const Variant &v);
+		Variant &operator=(const String &v);
+		Variant &operator=(const Float &v);
+		Variant &operator=(const Double &v);
+		Variant &operator=(const Int8 &v);
+		Variant &operator=(const Int16 &v);
+		Variant &operator=(const Int32 &v);
+		Variant &operator=(const Int64 &v);
+		Variant &operator=(const UInt8 &v);
+		Variant &operator=(const UInt16 &v);
+		Variant &operator=(const UInt32 &v);
+		Variant &operator=(const UInt64 &v);
+		Variant &operator=(const VectorChar &v);
+		Variant &operator=(const Date &v);
+		Variant &operator=(const Time &v);
+		Variant &operator=(const VectorVariant &v);
+		Variant &operator=(const MapStringVariant &v);
+
+		// queries
+		operator String &();
+		operator Float &();
+		operator Double &();
+		operator Int8 &();
+		operator Int16 &();
+		operator Int32 &();
+		operator Int64 &();
+		operator UInt8 &();
+		operator UInt16 &();
+		operator UInt32 &();
+		operator UInt64 &();
+		operator VectorChar &();
+		operator Date &();
+		operator Time &();
+		operator VectorVariant &();
+		operator MapStringVariant &();
 
 
-		void swap(variant &);
-		Variant & operator=(const Variant &);
-		template<typename T> variant & operator=(const T &);
+		operator String const &() const;
+		operator Float const &() const;
+		operator Double const &() const;
+		operator Int8 const &() const;
+		operator Int16 const &() const;
+		operator Int32 const &() const;
+		operator Int64 const &() const;
+		operator UInt8 const &() const;
+		operator UInt16 const &() const;
+		operator UInt32 const &() const;
+		operator UInt64 const &() const;
+		operator VectorChar const &() const;
+		operator Date const &() const;
+		operator Time const &() const;
+		operator VectorVariant const &() const;
+		operator MapStringVariant const &() const;
 
-		// 	// queries
-		// 	int which() const;
-		// 	bool empty() const;
-		// 	const std::type_info & type() const;
+		void swap(Variant &);
+		bool empty() const;
+		EVariantType type() const;
 
 		template<typename T> T &as();
-		template<typename T> bool is();
+		template<typename T> const T &as() const;
+		template<typename T> bool is() const;
+		template<typename T> void forceType();
+
+	protected:
+		static const size_t _dataSize = 8;
+		char _data[_dataSize];
+		EVariantType _evt;
+
 	};
 }
 
--- runtime/src/utils/data.cpp
+++ runtime/src/utils/data.cpp
@@ -3,5 +3,186 @@
 
 namespace utils
 {
+	class VariantImpl
+		: private Variant
+	{
+	public:
+		template <class T>
+		void construct(const T &v)
+		{
+			assert(0);
+		}
+		void construct()
+		{
+			assert(0);
+		}
+		void destruct()
+		{
+			assert(0);
+		}
 
+		template <class T>
+		void assign(const T &v)
+		{
+			assert(0);
+		}
+
+		template <class T>
+		T &as()
+		{
+			assert(0);
+			static T t;
+			return t;
+		}
+
+		template <class T>
+		const T &as() const
+		{
+			assert(0);
+			static T t;
+			return t;
+		}
+
+		template<typename T> 
+		bool is() const
+		{
+			assert(0);
+			return false;
+		}
+
+		template<typename T> 
+		void forceType()
+		{
+			assert(0);
+		}
+	};
+#define IMPL ((VariantImpl *)this)
+
+	//////////////////////////////////////////////////////////////////////////
+	Variant::~Variant()
+	{
+		IMPL->destruct();
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	Variant::Variant()
+	{
+		IMPL->construct();
+	}
+
+
+
+
+
+
+
+	//////////////////////////////////////////////////////////////////////////
+#define ENUMTYPES					\
+	ENUMTYPES_ONE(String)			\
+	ENUMTYPES_ONE(Float)			\
+	ENUMTYPES_ONE(Double)			\
+	ENUMTYPES_ONE(Int8)				\
+	ENUMTYPES_ONE(Int16)			\
+	ENUMTYPES_ONE(Int32)			\
+	ENUMTYPES_ONE(Int64)			\
+	ENUMTYPES_ONE(UInt8)			\
+	ENUMTYPES_ONE(UInt16)			\
+	ENUMTYPES_ONE(UInt32)			\
+	ENUMTYPES_ONE(UInt64)			\
+	ENUMTYPES_ONE(VectorChar)		\
+	ENUMTYPES_ONE(Date)				\
+	ENUMTYPES_ONE(Time)				\
+	ENUMTYPES_ONE(VectorVariant)	\
+	ENUMTYPES_ONE(MapStringVariant)	//\
+
+
+
+
+
+
+
+	//////////////////////////////////////////////////////////////////////////
+#define ENUMTYPES_ONE(T)			\
+	Variant::Variant(const T &v)	\
+	{								\
+			IMPL->construct(v);		\
+	}								//\
+
+ENUMTYPES_ONE(Variant)
+ENUMTYPES
+#undef ENUMTYPES_ONE
+
+//////////////////////////////////////////////////////////////////////////
+#define ENUMTYPES_ONE(T)					\
+	Variant &Variant::operator=(const T &v)	\
+	{										\
+		IMPL->assign(v);					\
+		return *this;						\
+	}										//\
+
+ENUMTYPES_ONE(Variant)
+ENUMTYPES
+#undef ENUMTYPES_ONE
+
+//////////////////////////////////////////////////////////////////////////
+#define ENUMTYPES_ONE(T)			\
+	Variant::operator T &()			\
+	{								\
+		return IMPL->as<T>();		\
+	}								//\
+
+ENUMTYPES
+#undef ENUMTYPES_ONE
+
+
+//////////////////////////////////////////////////////////////////////////
+#define ENUMTYPES_ONE(T)					\
+	Variant::operator T const &() const		\
+	{										\
+		return IMPL->as<T>();				\
+	}										//\
+
+ENUMTYPES
+#undef ENUMTYPES_ONE
+
+
+	//////////////////////////////////////////////////////////////////////////
+	void Variant::swap(Variant & v)
+	{
+		std::swap(_evt, v._evt);
+		//std::swap(_data, v._data);
+		assert(0);
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	bool Variant::empty() const
+	{
+		return evtNull == _evt;
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	EVariantType Variant::type() const
+	{
+		return _evt;
+	}
+
+	template<typename T> T &Variant::as()
+	{
+		return IMPL->as<T>();
+	}
+
+	template<typename T> const T &Variant::as() const
+	{
+		return IMPL->as<T>();
+	}
+
+	template<typename T> bool Variant::is() const
+	{
+		return IMPL->is<T>();
+	}
+
+	template<typename T> void Variant::forceType()
+	{
+		return IMPL->forceType<T>();
+	}
 }
