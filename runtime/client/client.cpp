095d08df26255aa0636a7686fce5d49f5a119d1a
vopl <devnull@localhost>
2011-11-17 00:00:00 +0400

    проект клиента вычещен

--- runtime/client/client.cpp
+++ runtime/client/client.cpp
@@ -3,22 +3,16 @@
 
 Client::Client(QWidget *parent, Qt::WFlags flags)
 	: QMainWindow(parent, flags)
-	, _connection(NULL)
 {
 	ui.setupUi(this);
 
 	ui.actionFullScreen->setShortcut(QKeySequence(Qt::CTRL+Qt::Key_F11));
 	connect(ui.actionFullScreen, SIGNAL(changed()),
 		this, SLOT(onFullScreen()));
-
-	_connection = new r3::logic::client::Connection(this);
-	setCentralWidget(_connection);
 }
 
 Client::~Client()
 {
-	_connection->deleteLater();
-	_connection = 0;
 }
 
 void Client::resizeEvent(QResizeEvent *)
--- runtime/client/client.h
+++ runtime/client/client.h
@@ -3,15 +3,12 @@
 
 #include <QtGui/QMainWindow>
 #include "ui_client.h"
-#include "connection.hpp"
 
 class Client : public QMainWindow
 {
 	Q_OBJECT
 
 private:
-
-	r3::logic::client::Connection *_connection;
 
 public:
 	Client(QWidget *parent = 0, Qt::WFlags flags = 0);
