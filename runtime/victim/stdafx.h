1969a9212782a4773cd11298b2b90cf91a1e24e0
vopl <devnull@localhost>
2011-11-22 00:00:00 +0400

    .

--- runtime/victim/stdafx.h
+++ runtime/victim/stdafx.h
@@ -18,9 +18,8 @@
 #include <vector>
 #include <deque>
 #include <string>
-
 #include <cassert>
 
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
 
-#include <boost/foreach.hpp>
-#include "pgs/meta/common.hpp"
