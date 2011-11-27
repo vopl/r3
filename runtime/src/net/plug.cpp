ec2654f00ee8cfb254a1883b5f6ac0c46df9fbf9
sash <sash@station4>
2011-11-28 00:00:00 +0400

    .

--- runtime/src/net/plug.cpp
+++ runtime/src/net/plug.cpp
@@ -1,13 +1,11 @@
 #include "pch.h"
 #include "pluma/connector.hpp"
 #include "connector.hpp"
-#include "clientSession.hpp"
 
 
 PLUMA_CONNECTOR
 bool pluma_connect(pluma::Host& host)
 {
 	host.add( new net::ConnectorProvider() );
-	host.add( new net::ClientSessionProvider() );
 	return true;
 }
