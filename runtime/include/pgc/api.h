1bd2ce19e0d8199cc9ccb25d0d93395e92a2fab5
vopl <devnull@localhost>
2012-02-13 00:00:00 +0400

    .

--- runtime/include/pgc/api.h
+++ runtime/include/pgc/api.h
@@ -1,4 +1,9 @@
+﻿/*!	\defgroup pg Постгрес клиент (pg)
 
+	Предоставляет 2 уровня доступа к БД PostgreSQL:
+		- pgc (postgres client) - низкоуровневый интерфейс, соединится/отсоединится, выполнить SQL запрос, получить данные
+		- pgs (postgres structure) - более высокий уровень, аля orm, еще не допилен
+*/
 #if defined OS_WINDOWS
 #   ifdef libpg_EXPORTS
 #	    define PG_API __declspec(dllexport)
